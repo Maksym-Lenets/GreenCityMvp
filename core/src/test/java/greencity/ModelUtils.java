5f167762b4e6ea8be89147832803bf9a605f7fc5
Yurii Midianyi <45738210+Yurii-Midianyi@users.noreply.github.com>
2023-07-16 11:10:15 +0300

    deleted not needed controllers and changed ModelUtils (#13)

--- core/src/test/java/greencity/ModelUtils.java
+++ core/src/test/java/greencity/ModelUtils.java
@@ -91,7 +91,6 @@ import greencity.enums.TagType;
 import greencity.enums.UserStatus;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
